53d393c0931c401c33425be4484c9c6d933f3e4f
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-04-21 20:56:25 +0300

    Minimal working mod

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using IPA;
+using Logger = IPA.Logging.Logger;
+
+namespace BeatSaberTimeTracker
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    internal class Plugin
+    {
+        public static Logger logger { get; private set; }
+
+        [Init]
+        public Plugin(Logger logger)
+        {
+            Plugin.logger = logger;
+            logger.Debug("Init");
+        }
+
+        [OnStart]
+        public void OnStart()
+        {
+            logger.Debug("OnStart");
+        }
+
+        [OnExit]
+        public void OnExit()
+        {
+            logger.Debug("OnExit");
+        }
+    }
+}
